93e0f01be8095e6d2ef475f62ea4fe0dd5bc77dd
Your Namemcconejero <centenoconejeromario@gmail.com>
2019-01-22 10:17:04 +0100

    proyecto unido

---
+++ EcoHuerto-Juntado/loginEcoHuerto/app/src/main/java/com/rcarmona/loginecohuerto/InicioActivity.java
@@ -0,0 +1,16 @@
+package com.rcarmona.loginecohuerto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class InicioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.Tollbar);
+        setSupportActionBar(toolbar);
+    }
+}
