958e7902e39d966e577a4a83967eed5e963f4b5b
Your Namemcconejero <centenoconejeromario@gmail.com>
2019-02-04 14:23:54 +0100

    login y registro cambiado

--- Plantillas/app/src/main/java/com/mcenteno/plantillas/MainActivity.java
+++ Plantillas/app/src/main/java/com/mcenteno/plantillas/MainActivity.java
@@ -1,15 +1,11 @@
 package com.mcenteno.plantillas;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView btnSignin;
     Button btnLogin;
 
     @Override
@@ -17,24 +13,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        btnSignin = findViewById(R.id.signin);
-        btnLogin = findViewById(R.id.loginButton);
 
-        btnSignin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intentRegistro = new Intent(MainActivity.this, RegistroActivity.class);
-                startActivity(intentRegistro);
-            }
-        });
+        btnLogin = findViewById(R.id.buttonLogin);
 
-        btnLogin.setOnClickListener(new View.OnClickListener() {
+
+        /*btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intentLogeo = new Intent(MainActivity.this, InicioActivity.class);
                 startActivity(intentLogeo);
             }
-        });
+        });*/
 
     }
 }
