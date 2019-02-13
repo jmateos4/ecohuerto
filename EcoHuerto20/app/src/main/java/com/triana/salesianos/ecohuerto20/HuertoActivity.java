1da3c2456987f8254e05f721d2878d46fa44cd2d
Your Namemcconejero <centenoconejeromario@gmail.com>
2019-02-13 08:42:24 +0100

    ok

--- EcoHuerto20/app/src/main/java/com/triana/salesianos/ecohuerto20/HuertoActivity.java
+++ EcoHuerto20/app/src/main/java/com/triana/salesianos/ecohuerto20/HuertoActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -14,7 +13,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
 import android.widget.Toast;
 
 import com.triana.salesianos.ecohuerto20.interfaces.HuertoInteractionListener;
