ccbfe90eb433b9caa367ee65a2d4c3e6c8582028
gonzalosm99 <gonzalosantiagomartin@gmail.com>
2019-02-14 13:37:17 +0100

    subo cambios en los modelos y plantaciones

--- EcoHuerto20/app/src/main/java/com/triana/salesianos/ecohuerto20/CentroMeteorologico.java
+++ EcoHuerto20/app/src/main/java/com/triana/salesianos/ecohuerto20/CentroMeteorologico.java
@@ -8,8 +8,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class UserFragment extends Fragment {
 
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link CentroMeteorologico.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link CentroMeteorologico#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class CentroMeteorologico extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -19,13 +27,23 @@ public class UserFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    private Context ctx;
+    private OnFragmentInteractionListener mListener;
 
-    public UserFragment() { }
+    public CentroMeteorologico() {
+        // Required empty public constructor
+    }
 
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment CentroMeteorologico.
+     */
     // TODO: Rename and change types and number of parameters
-    public static UserFragment newInstance(String param1, String param2) {
-        UserFragment fragment = new UserFragment();
+    public static CentroMeteorologico newInstance(String param1, String param2) {
+        CentroMeteorologico fragment = new CentroMeteorologico();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -46,31 +64,45 @@ public class UserFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user, container, false);
+        return inflater.inflate(R.layout.fragment_centro_meteorologico, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
-        /*if (mListener != null) {
+        if (mListener != null) {
             mListener.onFragmentInteraction(uri);
-        }*/
+        }
     }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        this.ctx = context;
-        /*if (context instanceof OnFragmentInteractionListener) {
+        if (context instanceof OnFragmentInteractionListener) {
             mListener = (OnFragmentInteractionListener) context;
         } else {
             throw new RuntimeException(context.toString()
                     + " must implement OnFragmentInteractionListener");
-        }*/
+        }
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        //mListener = null;
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
     }
 }
