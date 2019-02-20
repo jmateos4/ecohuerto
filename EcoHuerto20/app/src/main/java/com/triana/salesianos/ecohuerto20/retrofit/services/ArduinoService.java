09b1dbf747563a86842c8a2c9dd2a233dd5e1930
gonzalosm99 <gonzalosantiagomartin@gmail.com>
2019-02-20 09:01:31 +0100

    Ultimo merge resuelto

--- EcoHuerto20/app/src/main/java/com/triana/salesianos/ecohuerto20/retrofit/services/ArduinoService.java
+++ EcoHuerto20/app/src/main/java/com/triana/salesianos/ecohuerto20/retrofit/services/ArduinoService.java
@@ -1,11 +1,8 @@
 package com.triana.salesianos.ecohuerto20.retrofit.services;
 
-<<<<<<< HEAD
 import android.support.v7.widget.CardView;
 
 import okhttp3.ResponseBody;
-=======
->>>>>>> 0b7999a9d08489f09f014df32046cfbad3376874
 import retrofit2.Call;
 import retrofit2.http.GET;
 
